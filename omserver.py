e8793e7f8b1c4804190371a3b286eaabc99c17e9
Andrew Kirik <andykirik@users.noreply.githum.com>
2017-11-01 23:13:13 -0400

    get mac address

--- omserver.py
+++ omserver.py
@@ -9,17 +9,28 @@ import ommlib.httpclient as hc
 import utils.fileutils as fu
 from time import gmtime, strftime
 import os.path
+import socket
+import fcntl
+import struct
+import netifaces
 
 #logger = logging.getLogger(__name__)
 logger = logging.getLogger('omserver')
 
 def initLogger():
-    logging.basicConfig(level=logging.ERROR)
+    #logging.basicConfig(level=logging.ERROR)
     handler = logging.handlers.RotatingFileHandler("omserver.log", 'a', 1024, 3)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
+def getMacAddress():
+    ifname = netifaces.interfaces()[1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    address = ':'.join(['%02x' % ord(char) for char in info[18:24]])
+    return address.replace(':','-')
 
 def main():
     srTime = strftime("%Y%m%d%H%M", gmtime())
@@ -35,6 +46,8 @@ def main():
     db.getSiteID()
     hc.licenseCheck()
     logger.info(fu.getHash(""))
+    #logger.info(getHwAddr("ens33"))
+    print (getMacAddress())
 
 
 if __name__ == '__main__':
